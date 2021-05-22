7a1630148e8e0eda7ec623c33ab8a9e735dad544
Adam Banaszkiewicz <adam@tuliacms.com>
2021-05-22 13:46:57 +0200

    Refactoring taxonomies Breadcrumbs

---
+++ src/Cms/Taxonomy/Infrastructure/Persistence/Domain/ReadModel/DbalTaxonomyBreadcrumbsReadStorage.php
@@ -0,0 +1,88 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tulia\Cms\Taxonomy\Infrastructure\Persistence\Domain\ReadModel;
+
+use Tulia\Cms\Shared\Ports\Infrastructure\Persistence\DBAL\ConnectionInterface;
+use Tulia\Cms\Taxonomy\Ports\Infrastructure\Persistence\Domain\ReadModel\TaxonomyBreadcrumbsReadStorageInterface;
+
+/**
+ * @author Adam Banaszkiewicz
+ */
+class DbalTaxonomyBreadcrumbsReadStorage implements TaxonomyBreadcrumbsReadStorageInterface
+{
+    private ConnectionInterface $connection;
+
+    public function __construct(ConnectionInterface $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    public function find(string $termId, string $websiteId, string $locale, string $defaultLocale): array
+    {
+        return $this->connection->fetchAllAssociative("
+WITH RECURSIVE tree_path (
+    id,
+    website_id,
+    type,
+    is_root,
+    parent_id,
+    position,
+    level,
+    count,
+    locale,
+    name,
+    slug,
+    visibility,
+    generated_path
+) AS (
+    SELECT
+        id,
+        website_id,
+        type,
+        is_root,
+        parent_id,
+        position,
+        level,
+        count,
+        :defaultLocale AS locale,
+        name,
+        slug,
+        visibility,
+        CONCAT(name, '/') as generated_path
+    FROM #__term
+    WHERE
+        id = :term_id
+        AND website_id = :websiteId
+UNION ALL
+    SELECT
+            tm.id,
+            tm.website_id,
+            tm.type,
+            tm.is_root,
+            tm.parent_id,
+            tm.position,
+            tm.level,
+            tm.count,
+            COALESCE(tl.locale, :defaultLocale) AS locale,
+            COALESCE(tl.name, tm.name) AS name,
+            COALESCE(tl.slug, tm.slug) AS slug,
+            COALESCE(tl.visibility, tm.visibility) AS visibility,
+            CONCAT(tp.generated_path, tm.name, '/') AS generated_path
+        FROM tree_path AS tp
+        INNER JOIN #__term AS tm
+            ON tp.parent_id = tm.id
+        LEFT JOIN #__term_lang AS tl
+            ON tm.id = tl.term_id AND tl.locale = :locale
+        WHERE
+            tm.website_id = :websiteId
+)
+SELECT * FROM tree_path", [
+            'term_id' => $termId,
+            'websiteId' => $websiteId,
+            'locale' => $locale,
+            'defaultLocale' => $defaultLocale,
+        ]);
+    }
+}
--- src/Cms/Taxonomy/UserInterface/Web/Frontend/Breadcrumbs/CrumbsResolver.php
+++ src/Cms/Taxonomy/UserInterface/Web/Frontend/Breadcrumbs/CrumbsResolver.php
@@ -6,6 +6,7 @@ namespace Tulia\Cms\Taxonomy\UserInterface\Web\Frontend\Breadcrumbs;
 
 use Symfony\Component\Routing\RouterInterface;
 use Tulia\Cms\Breadcrumbs\Application\Crumbs\ResolverInterface;
+use Tulia\Cms\Taxonomy\Domain\ReadModel\TaxonomyBreadcrumbs;
 use Tulia\Cms\Taxonomy\Domain\TaxonomyType\RegistryInterface;
 use Tulia\Cms\Taxonomy\Domain\ReadModel\Finder\Enum\TermFinderScopeEnum;
 use Tulia\Cms\Taxonomy\Domain\ReadModel\Finder\Model\Term;
@@ -18,20 +19,24 @@ use Symfony\Component\HttpFoundation\Request;
  */
 class CrumbsResolver implements ResolverInterface
 {
-    protected RouterInterface $router;
+    private RouterInterface $router;
 
-    protected RegistryInterface $typeRegistry;
+    private RegistryInterface $typeRegistry;
 
-    protected TermFinderInterface $termFinder;
+    private TermFinderInterface $termFinder;
+
+    private TaxonomyBreadcrumbs $taxonomyBreadcrumbs;
 
     public function __construct(
         RouterInterface $router,
         RegistryInterface $typeRegistry,
-        TermFinderInterface $termFinder
+        TermFinderInterface $termFinder,
+        TaxonomyBreadcrumbs $taxonomyBreadcrumbs
     ) {
         $this->router = $router;
         $this->typeRegistry = $typeRegistry;
         $this->termFinder = $termFinder;
+        $this->taxonomyBreadcrumbs = $taxonomyBreadcrumbs;
     }
 
     public function findRootCrumb(Request $request): ?object
@@ -45,18 +50,21 @@ class CrumbsResolver implements ResolverInterface
             : null;
     }
 
+    /**
+     * @param Term $term
+     * @param BreadcrumbsInterface $breadcrumbs
+     * @return object|null
+     */
     public function fillBreadcrumbs(object $term, BreadcrumbsInterface $breadcrumbs): ?object
     {
-        /** @var Term $term */
+        $path = $this->taxonomyBreadcrumbs->find($term->getId());
 
-        $breadcrumbs->unshift($this->router->generate('term_' . $term->getId()), $term->getName());
-
-        if ($this->typeRegistry->isTypeRegistered($term->getType())) {
-            $type = $this->typeRegistry->getType($term->getType());
-
-            if ($type->supports('hierarchy') && $term->getParentId()) {
-                $this->resolveHierarchyCrumbs($breadcrumbs, $term);
+        foreach ($path as $crumb) {
+            if ($crumb->isRoot()) {
+                continue;
             }
+
+            $breadcrumbs->unshift($this->router->generate('term_' . $crumb->getId()), $crumb->getName());
         }
 
         return null;
@@ -65,26 +73,5 @@ class CrumbsResolver implements ResolverInterface
     public function supports(object $identity): bool
     {
         return $identity instanceof Term;
-    }
-
-    private function resolveHierarchyCrumbs(BreadcrumbsInterface $breadcrumbs, Term $term): void
-    {
-        $parentId = $term->getParentId();
-        $terms = [];
-
-        while ($parentId) {
-            $parent = $this->termFinder->findOne(['id' => $parentId], TermFinderScopeEnum::BREADCRUMBS);
-
-            if ($parent) {
-                $terms[] = $parent;
-                $parentId = $parent->getParentId();
-            } else {
-                $parentId = null;
-            }
-        }
-
-        foreach ($terms as $parent) {
-            $breadcrumbs->unshift($this->router->generate('term_' . $parent->getId()), $parent->getName());
-        }
     }
 }
