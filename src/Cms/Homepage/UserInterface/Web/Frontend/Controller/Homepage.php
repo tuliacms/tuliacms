8848e3f60bb88be61a652d5eceb4540b8db09c3e
Adam Banaszkiewicz <adam@tuliacms.com>
2022-02-28 19:36:38 +0100

    Optimize Homepage Node rendering

--- src/Cms/Homepage/UserInterface/Web/Frontend/Controller/Homepage.php
+++ src/Cms/Homepage/UserInterface/Web/Frontend/Controller/Homepage.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Tulia\Cms\Homepage\UserInterface\Web\Frontend\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Tulia\Cms\Node\Domain\NodeFlag\Enum\NodeFlagEnum;
 use Tulia\Cms\Node\Domain\ReadModel\Finder\NodeFinderInterface;
@@ -18,25 +19,25 @@ use Tulia\Component\Templating\ViewInterface;
 class Homepage extends AbstractController
 {
     private NodeFinderInterface $nodeFinder;
+    private Node $nodeController;
 
-    public function __construct(NodeFinderInterface $nodeFinder)
+    public function __construct(NodeFinderInterface $nodeFinder, Node $nodeController)
     {
         $this->nodeFinder = $nodeFinder;
+        $this->nodeController = $nodeController;
     }
 
     /**
      * @return Response|ViewInterface
      */
-    public function index()
+    public function index(Request $request)
     {
         $homepage = $this->nodeFinder->findOne([
             'flag' => NodeFlagEnum::PAGE_HOMEPAGE,
         ], NodeFinderScopeEnum::SINGLE);
 
         if ($homepage) {
-            return $this->forward(Node::class . '::show', [
-                'node' => $homepage,
-            ]);
+            return $this->nodeController->show($homepage, $request);
         }
 
         return $this->view('@cms/homepage/index.tpl');
