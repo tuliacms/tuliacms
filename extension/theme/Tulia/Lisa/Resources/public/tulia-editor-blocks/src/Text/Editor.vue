c45e14cb5ab307cf708618251a96d1ee8c3744b4
Adam Banaszkiewicz <adam@tuliacms.com>
2022-09-13 20:04:17 +0200

    Updates

--- extension/theme/Tulia/Lisa/Resources/public/tulia-editor-blocks/src/Text/Editor.vue
+++ extension/theme/Tulia/Lisa/Resources/public/tulia-editor-blocks/src/Text/Editor.vue
@@ -1,14 +1,8 @@
 <template>
     <div class="block block-text">
-        <div class="container-xxl">
-            <div class="row">
-                <div class="col">
-                    <p :class="{ 'lead': true, 'text-center': block.data.headline_justify === 'center', 'text-end': block.data.headline_justify === 'right' }"><Contenteditable v-model="block.data.intro"></Contenteditable></p>
-                    <h2 :class="{ 'text-center': block.data.headline_justify === 'center', 'text-end': block.data.headline_justify === 'right' }"><Contenteditable v-model="block.data.headline"></Contenteditable></h2>
-                    <WysiwygEditor v-model="block.data.content"></WysiwygEditor>
-                </div>
-            </div>
-        </div>
+        <p :class="{ 'lead': true, 'text-center': block.data.headline_justify === 'center', 'text-end': block.data.headline_justify === 'right' }"><Contenteditable v-model="block.data.intro"></Contenteditable></p>
+        <h2 :class="{ 'text-center': block.data.headline_justify === 'center', 'text-end': block.data.headline_justify === 'right' }"><Contenteditable v-model="block.data.headline"></Contenteditable></h2>
+        <WysiwygEditor v-model="block.data.content"></WysiwygEditor>
     </div>
 </template>
 
